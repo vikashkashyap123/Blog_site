2949d8828d1e98bc3d393ca5536e822119c5f4c6
vikash kumar <vikashkashyap14955@gmail.com>
2024-07-17 00:53:37 +0530

    dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 
 export default function Header() {
   const path=useLocation().pathname;
+  const dispatch = useDispatch();
   const {currentUser} = useSelector(state => state.user);
+  const {theme} = useSelector((state)=>state.theme);
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -26,8 +29,9 @@ export default function Header() {
           <AiOutlineSearch/>
         </Button>
         <div className='flex gap-2 md:order-2'>
-          <Button className='w-12 h-10 hidden sm:inline' color='gray' pill='true'>
-            <FaMoon/>
+          <Button className='w-12 h-10 hidden sm:inline' color='gray' pill='true' onClick={()=> dispatch(toggleTheme())}>
+            {theme === 'light' ? <FaSun/> : <FaMoon/>}
+            
           </Button>
           {currentUser ? (
             <Dropdown 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.jsx';
+import ThemeProvider from './components/ThemeProvider.jsx';
 import './index.css';
 import { persistor, store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-    <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    
     </Provider>
   </PersistGate>
 );
