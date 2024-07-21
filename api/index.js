c0bad15edfede23b55a45f92082b046aeb6a2b64
vikash kumar <vikashkashyap14955@gmail.com>
2024-07-22 04:02:58 +0530

    add create a api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import userRoutes from './routes/user.route.js';
 
 
@@ -29,6 +30,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth',authRoutes);
+app.use('/api/post',postRoutes);
 
 app.use((err,req,res,next) =>{
     const statusCode = err.statusCode || 500;
