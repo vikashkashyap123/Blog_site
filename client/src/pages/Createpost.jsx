f1e8a958f21fb6579d6ccb5e93d34180ed08555c
vikash kumar <95958082+vikashkashyap123@users.noreply.github.com>
2024-07-22 18:50:34 +0530

    Update Createpost.jsx

--- client/src/pages/Createpost.jsx
+++ client/src/pages/Createpost.jsx
@@ -5,7 +5,7 @@ import 'react-quill/dist/quill.snow.css';
 
 export default function Createpost() {
   return (
-    <div className='p-3 max-w-3xl max-auto min-h-screen'>
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
         <form className='flex flex-col gap-4'>
             <div className='flex flex-col gap-4 sm:flex-row justify-between'>
