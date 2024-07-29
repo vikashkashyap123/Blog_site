35d13a431a368f304fb24c280090539a29b1c2c5
vikash kumar <vikashkashyap14955@gmail.com>
2024-07-30 01:46:06 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import commentRoutes from "./routes/comment.route.js";
 import postRoutes from './routes/post.route.js';
@@ -20,6 +21,8 @@ mongoose.connect(
     console.log(err);
 });
 
+const __dirname=path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -33,6 +36,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
 
 app.use((err,req,res,next) =>{
     const statusCode = err.statusCode || 500;
